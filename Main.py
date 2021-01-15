b80d632b9382eaaca87869646f1bd68ee760aea2
niko9083 <niko9083@edu.aarhustech.dk>
2021-01-15 13:32:07 +0100

    What is up, fellow gamers

--- Main.py
+++ Main.py
@@ -7,6 +7,7 @@ windowWidth = 800
 windowHeight = 500
 clock = pygame.time.Clock()
 
+font =pygame.font.SysFont("Arial", 20)
 
 while not done:
     for event in pygame.event.get():
@@ -17,12 +18,15 @@ while not done:
 
     mouseX, mouseY = pygame.mouse.get_pos()
 
-    print(mouseX, ",", mouseY)
+    xText = font.render(f"x: {mouseX}", False, (0, 0, 255))
+    yText = font.render(f"y: {mouseY}", False, (0, 255, 0))
 
     screen.fill((0, 0, 0))
 
-    pygame.draw.rect(screen, (255, 255, 255), (0, mouseY, windowWidth, 1))
-    pygame.draw.rect(screen, (255, 255, 255), (mouseX, 0, 1, windowHeight))
+    pygame.draw.rect(screen, (0, 0, 255), (mouseX, 0, 1, windowHeight))
+    pygame.draw.rect(screen, (0, 255, 0), (0, mouseY, windowWidth, 1))
+    screen.blit(xText, (0, 0))
+    screen.blit(yText, (0, 20))
 
     pygame.display.flip()
     clock.tick(60)
